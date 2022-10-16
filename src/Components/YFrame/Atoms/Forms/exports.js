8e6339d661233e314ef3b808fd8debbf5ce8dfab
rob <dev@mexion.com>
2022-10-16 16:22:02 +0200

    move formvalidated to ui

--- src/Components/YFrame/Atoms/Forms/exports.js
+++ src/Components/YFrame/Atoms/Forms/exports.js
@@ -1,5 +1,9 @@
-export * from "./Auth/exports.js"
 export * from "./Buttons/exports.js"
 export * from "./Checks/exports.js"
 export * from "./Inputs/exports.js"
 export * from "./Register/exports.js"
+import YFormValidated from "./Validation/FormValidated.vue"
+
+export {
+    YFormValidated,
+}
