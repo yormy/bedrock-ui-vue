1e5cd45c9cb32d6da2de9c9f05a411c53d90425e
rob <rob@prigroup.eu>
2022-10-22 16:01:39 +0200

    moved to validation folder

--- src/Components/YFrame/Atoms/Forms/Validation/exports.js
+++ src/Components/YFrame/Atoms/Forms/Validation/exports.js
@@ -1,5 +1,7 @@
 import YValidationInput from "./ValidationInput.vue"
+import YFormValidated from "./FormValidated.vue"
 
 export {
     YValidationInput,
+    YFormValidated,
 }
--- src/Components/YFrame/Atoms/Forms/exports.js
+++ src/Components/YFrame/Atoms/Forms/exports.js
@@ -2,8 +2,4 @@ export * from "./Buttons/exports.js"
 export * from "./Checks/exports.js"
 export * from "./Inputs/exports.js"
 export * from "./Register/exports.js"
-import YFormValidated from "./Validation/FormValidated.vue"
-
-export {
-    YFormValidated,
-}
+export * from "./Validation/exports.js"
